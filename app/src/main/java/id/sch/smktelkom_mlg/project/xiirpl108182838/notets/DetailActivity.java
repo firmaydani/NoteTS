5f82f0d03e51d0ce417f5b04848056caa136e5ed
zhorif <zhorifrohman@gmail.com>
2016-11-14 08:10:55 +0700

    Commit Pertama

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl108182838/notets/DetailActivity.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.project.xiirpl108182838.notets;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl108182838/notets/helper/DBAdapter.java
@@ -0,0 +1,8 @@
+package id.sch.smktelkom_mlg.project.xiirpl108182838.notets.helper;
+
+/**
+ * Created by SMK Telkom SP Malang on 14/11/2016.
+ */
+
+public class DBAdapter {
+}
